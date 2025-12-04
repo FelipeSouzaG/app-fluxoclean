5e2a1cfc42e6ffae728dfbbf123799659bf502d9
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2025-12-04 20:55:00 -0300

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+const isProd = (import.meta as any).env.PROD;
+
+export const FLUXOCLEAN_API = isProd
+  ? 'https://api-fluxoclean.onrender.com/api'
+  : 'http://localhost:4000/api';
+
+export const FLUXOCLEAN_LOGIN = isProd
+  ? 'https://fluxoclean.com.br/login'
+  : 'http://localhost:3000/login';
