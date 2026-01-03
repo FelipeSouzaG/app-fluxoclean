2838b2da85c4c72dbed4e31b453b017e99bc95d8
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2026-01-03 20:10:05 -0300

    Delete src directory

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-const isProd = (import.meta as any).env.PROD;
-
-export const FLUXOCLEAN_API = isProd
-  ? 'https://api-fluxoclean.onrender.com/api'
-  : 'http://localhost:4000/api';
-
-export const FLUXOCLEAN_LOGIN = isProd
-  ? 'https://fluxoclean.com.br/login'
-  : 'http://localhost:3000/login';
