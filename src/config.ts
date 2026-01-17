b203e9b5ca5feddd986acd6c746ae0cc404a4a94
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2026-01-17 17:51:08 -0300

    Delete src directory

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const FLUXOCLEAN_API = 'https://api.fluxoclean.com.br/api';
-export const FLUXOCLEAN_LOGIN = 'https://fluxoclean.com.br/login';
-export const FLUXOCLEAN_HOME = 'https://fluxoclean.com.br';
